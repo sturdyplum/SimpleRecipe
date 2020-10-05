bd6bcb0e753eaffeee6ac008257a89bdfbe242d3
sturdyplum <pabbelt@gmail.com>
2020-10-05 00:43:39 -0700

    Small bugfix

--- background.js
+++ background.js
@@ -2,7 +2,7 @@ chrome.runtime.onInstalled.addListener(function() {
     chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
       chrome.declarativeContent.onPageChanged.addRules([{
         conditions: [new chrome.declarativeContent.PageStateMatcher({
-          pageUrl: {hostEquals: ''},
+          pageUrl: {hostContains: ''},
         })
         ],
             actions: [new chrome.declarativeContent.ShowPageAction()]
