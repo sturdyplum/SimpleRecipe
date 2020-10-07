cbc890b84567e18ba1a26b81266789d850333e2a
sturdyplum <pabbelt@gmail.com>
2020-10-07 15:05:36 -0700

    Add typescript support and convert index.js to typescript.

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,24 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './src/index.ts',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+
+  devtool: "source-map",
+
+  module: {
+    rules: [
+      // All files with a '.ts' or '.tsx' extension will be handled by 'ts-loader'.
+      { test: /\.tsx?$/, loader: "ts-loader" },
+
+      // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+      { test: /\.js$/, loader: "source-map-loader" }
+    ]
+  },
+
   node: {
     net: 'empty',
     tls: 'empty',
